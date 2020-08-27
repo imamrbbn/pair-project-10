282a3cde9033bd7640d8c933f4a50db5666e20f4
Fandi Ario <fandi.ario10@gmail.com>
2020-08-27 16:08:56 +0700

    Release 0 -Data Traveller

---
+++ models/traveller.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Traveller extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Traveller.init({
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    asal: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Traveller',
+  });
+  return Traveller;
+};
